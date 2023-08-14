b00ca56f3238b89a2863a6d02993294dc2c0f4f5
Betelhemmekonnen <bmekonnenad@gmail.com>
2023-08-14 13:04:56 +0300

    add route to get comment

--- Backend/routes/posts.js
+++ Backend/routes/posts.js
@@ -1,9 +1,12 @@
 import express from "express"
-import {addpost,deletepost, getpost, getposts,updatepost} from "../controller/posts.js"
+import {addpost,deletepost, getpost, getposts,updatepost,addComment,getcomments} from "../controller/posts.js"
+//import { isAuthenticated } from "../controller/auth.js"
  const router = express.Router()
   router.get('/',getposts)
   router.get('/:id',getpost)
-  router.post('/',addpost)
+  router.post('/' ,addpost)
   router.delete('/:id', deletepost)
-  router.put('/:id', updatepost)
+  router.put('/:id',updatepost)
+  router.post('/comment/:id',addComment)
+  router.get('/comment/:id',getcomments)
 export default router
