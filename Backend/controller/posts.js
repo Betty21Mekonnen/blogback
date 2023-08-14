9fbc561b3070d213582ae087b73bd0c8adb0caa0
Betelhemmekonnen <bmekonnenad@gmail.com>
2023-08-14 13:02:11 +0300

    add functionality on add component get comment update posts

--- Backend/controller/posts.js
+++ Backend/controller/posts.js
@@ -1,5 +1,5 @@
 import {db} from '../db.js'
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 export const getposts=(req,res)=>{
 	const q= req.query.cat? "SELECT*FROM posts WHERE cat=?" : "SELECT * FROM posts";
     db.query(q,[req.query.cat],(err,data)=>{
@@ -8,7 +8,7 @@ export const getposts=(req,res)=>{
 	}) 
 }
 export const getpost=(req,res)=>{
-const q="SELECT p.id, `username`, `title`, `descr` , p.img,u.img AS userImg ,`cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id=?"
+const q="SELECT p.id,u.id AS userId , `username`, `title`, `descr` , p.img,u.img AS userImg ,`cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id=?"
   db.query(q,[req.params.id],(err,data)=>{
 	if(err) console.log(err)
 	//return res.status(500).json(err);
@@ -19,22 +19,90 @@ const q="SELECT p.id, `username`, `title`, `descr` , p.img,u.img AS userImg ,`ca
 }
 
 export const addpost=(req,res)=>{
-	res.json("this is from post controller ")
+	 const token =  req.cookies.token
+	 //console.log(token)
+	 if(!token) return res.status(401).json("Not authenticated user")
+	  jwt.verify(token,"jwtnewkey" , (err,userInfo)=>{
+		if (err) return res.status(403).json("Token is not valid")
+	const q = "INSERT INTO posts (title, descr, img, cat, date, uid) VALUES (?)";
+const values =[
+     req.body.title,
+	 req.body.descr,
+	 req.body.img,
+	 req.body.cat,
+	 req.body.date,
+	 userInfo.id
+]
+db.query(q,[values],(err,data)=>{
+	if(err) return res.status(500).json(err)
+	return res.json("post has been created")
+})
+	 })
 }
-export const deletepost=(req,res)=>{
-	const token =  req.cookies.newtoken 
-	console.log(req)
+export const deletepost = (req, res) => {
+	const token = req.cookies.token;
+	if (!token) return res.status(401).json("Not authenticated user");
+	
+	jwt.verify(token, "jwtnewkey", (err, userInfo) => {
+	  if (err) return res.status(403).json("Token is not valid");
+  
+	  const postId = req.params.id;
+	  const q = "DELETE FROM posts WHERE `id`=? AND `uid`=?";
+	  db.query(q, [postId, userInfo.id], (err, data) => {
+		//console.log(userInfo)
+		if (err) {
+		  return res.status(500).json("Error deleting the post");
+		} else if (data.affectedRows === 0) {
+		  return res.status(403).json("You can only delete your own post");
+		} else {
+		  return res.json("Post has been deleted!");
+		}
+	  });
+	});
+  };
+export const updatepost=(req,res)=>{
+	const token = req.cookies.token;
+  if (!token) return res.status(401).json("Not authenticated!");
+
+  jwt.verify(token, "jwtnewkey", (err, userInfo) => {
+    if (err) return res.status(403).json("Token is not valid!");
+
+    const postId = req.params.id;
+    const q =  "UPDATE posts SET `title`=?,`descr`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
+
+    const values = [req.body.title, req.body.descr, req.body.img, req.body.cat];
+
+    db.query(q, [...values, postId, userInfo.id], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.json("Post has been updated.");
+    });
+  });
+}
+export const addComment=(req,res)=>{
+	const token =  req.cookies.token
+	const {comment,date}=req.body;
+	//console.log(token)
 	if(!token) return res.status(401).json("Not authenticated user")
 	 jwt.verify(token,"jwtnewkey" , (err,userInfo)=>{
-		if (err) return res.status(403).json("Token is not valid")
-		const postId = req.params.id;
-		const q="DELETE FROM posts WHERE `id`=? AND `uid`=?"
-		db.query(q,[postId , userInfo.id], (err,data)=>{
-		if(err) return res.status(403).json("you can delete only your post")
-		return res.json("post has benn deleted!")
-		})
+	   if (err) return res.status(403).json("Token is not valid")
+	   const postId = req.params.id;
+	   const q = "INSERT INTO comments (postid, uid, comment,date) VALUES (?)";//lebcha comment table mefter alebgn becaus ke and belay comment for one post endet handle yaregal
+const values =[postId ,userInfo.id,comment,date ]
+db.query(q,[values],(err,data)=>{
+   if(err) return res.status(500).json(err)
+   return res.json("post has been created")
+})
 	})
 }
-export const updatepost=(req,res)=>{
-	res.json("this is from post controller ")
-}
+export const getcomments=(req,res)=>{
+	const q="SELECT c.postid, `username`, `comment` FROM users u JOIN comments c ON u.id=c.uid WHERE c.postid=?"
+	  db.query(q,[req.params.id],(err,data)=>{
+		if(err){ console.log(err)
+	       return res.status(500).json(err);
+		}
+		  // console.log(data)
+		return res.status(200).json(data)
+	
+	  })
+	}
+	
