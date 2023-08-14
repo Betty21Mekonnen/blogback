2ef8febb2094e48a08787caa31cb6a88fac7fd02
Betelhemmekonnen <bmekonnenad@gmail.com>
2023-08-14 13:04:18 +0300

    get user info username profile img and all posts of user

--- Backend/controller/users.js
+++ Backend/controller/users.js
@@ -1,3 +1,18 @@
-export const users=(req,res)=>{
-	res.json("this is from user controller ")
+import { db } from "../db.js"
+export const getpost=(req,res)=>{
+    //const q="SELECT*FROM posts  WHERE uid=?"
+	//const q="SELECT p.id,u.id AS userId , `username`, `title`, `descr` , p.img,u.img AS userImg ,`cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.uid=?"
+	const q="SELECT p.id, u.id AS userId, u.`username`, p.`title`, p.`descr`, p.`img`, u.`img` AS userImg, p.`date`, IFNULL(c.commentCount, 0) AS commentCount FROM users u JOIN posts p ON u.id = p.uid LEFT JOIN (SELECT postid, COUNT(*) AS commentCount FROM comments GROUP BY postid) c ON p.id = c.postid WHERE p.uid = ?"
+	db.query(q,[req.params.id],(err,data)=>{
+		//console.log([req.params.id]) 
+        if(err) {
+            console.log(err)
+           return res.status(500).json(err);
+        }
+        //console.log(data.length)
+        return res.status(200).json(data)
+    })
+	//const q= "SELECT  p.* , u.username AS uname,u.img AS userImg, COUNT(c.comment) AS comment FROM posts p JOIN users u ON u.id = p.uid LEFT JOIN comments c ON c.postid = p.id WHERE p.uid = ?";
+ 
+
 }
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,3 +1,9 @@
 import express from "express"
- const router = express.Router()
+import { getpost} from "../controller/users.js"
+const router = express.Router()
+ router.get('/:id',getpost)
+// router.delete('/:uid', deleteprofile)
+// router.put('/:uid',updateprofile)
+// router.post('/:uid',addprofile)
+//upload profile emage and edit
 export default router
