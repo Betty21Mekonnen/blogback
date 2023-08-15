ad04a01472509211937f3c17473b5494fd55dc3b
Betelhemmekonnen <bmekonnenad@gmail.com>
2023-08-15 14:02:04 +0300

    routes to get delete ....of post

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,9 +1,9 @@
 import express from "express"
-import { getpost} from "../controller/users.js"
+import { getpost,updateprofile,deleteAccount,addprofile} from "../controller/users.js"
 const router = express.Router()
  router.get('/:id',getpost)
-// router.delete('/:uid', deleteprofile)
-// router.put('/:uid',updateprofile)
-// router.post('/:uid',addprofile)
-//upload profile emage and edit
+ router.delete('/:uid', deleteAccount)
+ router.put('/:uid',updateprofile)
+ router.post('/:uid',addprofile)
+//upload profile image and edit
 export default router
