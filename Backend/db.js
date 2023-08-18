b40ead7baa40e1cf81a4c9c32ea440f873c85d74
Betelhemmekonnen <bmekonnenad@gmail.com>
2023-08-18 15:51:14 +0300

    railway database

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,12 @@
 import mysql from "mysql"
 import dotenv from 'dotenv';
 dotenv.config();
-export const db=mysql.createConnection({
-	host:process.env.DB_HOST,
-	user:process.env.DB_USER,
-	password:process.env.DB_PASSWORD,
-	database:process.env.DB_DATABASE,
-})
+const urlDb=`mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+export const db=mysql.createConnection(urlDb)
+// 	{
+
+// 	// host:process.env.DB_HOST,
+// 	// user:process.env.DB_USER,
+// 	// password:process.env.DB_PASSWORD,
+// 	// database:process.env.DB_DATABASE,
+// }
