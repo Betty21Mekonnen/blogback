ccf532e005c07d62682feaa83c75c8adc69c0f90
Betelhemmekonnen <bmekonnenad@gmail.com>
2023-08-24 09:49:43 +0300

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -41,7 +41,14 @@ console.log(currentUser)
         {currentPosts.map((post) => (
           <div className="flex flex-col md:flex-row md:gap-20" key={post.id}>
             <div className="md:w-1/3">
-              <img className="w-full h-full" src={`${import.meta.env.VITE_BACKEND_URL}/upload/${post.img}`} alt="no" />
+            <img
+  className="w-full h-full"
+  src={`${import.meta.env.VITE_BACKEND_URL}/upload/${post.img}`}
+  alt="no"
+  onError={(e) => {
+    e.target.src = '../upload/1691744596797paintings.jpg'; // Replace with the path to your alternative image
+  }}
+/>
             </div>
             <div className="md:w-2/3">
               <Link to={`/post/${post.id}`}>
